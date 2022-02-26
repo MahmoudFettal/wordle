db2cc182b7f376c6768e3e997516f6354b65ded1
smauj <mahmoud.fettal@gmail.com>
2022-02-27 00:22:00 +0100

    version 1.6

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Box from "../Box";
 import words from "../../words";
 
-const correct = "FORCE";
+const correct = "CHANT";
 let defaulBoard = [];
 let defaultLetters = [];
 
